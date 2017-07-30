42a12608d703545f753b1dcd027ef4b0ae927c31
ashok1 <kingkoirala1@gmail.com>
2017-07-30 10:54:12 +0545

    new components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,35 +3,59 @@ import { NgModule } from '@angular/core';
 import { RouterModule, ActivatedRoute, Params }   from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { MemberComponent } from './member/member.component';
-import { TeamComponent } from './team/team.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ReportComponent } from './components/report/report.component';
+import { UserComponent } from './components/user/user.component';
+import { CustomPipe } from './pipes/custom.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MemberComponent,
-    TeamComponent
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    ReportComponent,
+    UserComponent,
+    CustomPipe
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       {
-        path: 'member/:id',
-        component: MemberComponent
+        path: 'login',
+        component: LoginComponent
       },
       {
-        path: 'member/',
-        component: MemberComponent
+        path: 'register',
+        component: RegisterComponent
       },
       {
-        path: 'team',
-        component: TeamComponent
+        path: '',
+        component: UserComponent,
+        children: [
+          {
+            path: '',
+            redirectTo: 'dashboard',
+            pathMatch: 'full'
+          },
+          {
+            path: 'dashboard',
+            component: DashboardComponent
+          },
+          {
+            path: 'report',
+            component: ReportComponent
+          },
+        ]
       }
 
     ])
-    ],
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
